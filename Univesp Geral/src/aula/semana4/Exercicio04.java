5e794e95ed30c97f070e5b1c8a508ae2048871cf
felipe-abruzzese <31362855+felipe-abruzzese@users.noreply.github.com>
2019-06-29 13:28:58 -0300

    aula

--- Univesp Geral/src/aula/semana4/Exercicio04.java
+++ Univesp Geral/src/aula/semana4/Exercicio04.java
@@ -1,9 +1,28 @@
 package aula.semana4;
 
+/*
+ * Escreva um método que desenhe um retângulo de "*" de n × m (n linhas × m colunas), n e m serão parâmetros inteiros do seu método. 
+ * Segue exemplo de 3 linhas e 4 colunas:
+ */
+
 public class Exercicio04 {
 
+	public static void retangulo(int n, int m) {
+		for (int i = 0; i <= n; i++) {
+			System.out.print("*");
+			for (int b = 0; b <= m; i++) {
+				System.out.print("*");
+				for (int a = 0; a + 2 <= m; i++) {
+					System.out.print(" ");
+				}
+
+			}
+			System.out.println("*");
+		}
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		retangulo(10, 10);
 
 	}
 
